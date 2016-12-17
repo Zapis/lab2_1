592c6ff833e148c87a2cf20f59e7146c4383d402
Marcin Zapisek <zapisekm@gmail.com>
2016-12-17 16:19:56 +0100

    v3

--- src/test/java/lab2_1/BinarySearchSekwencjaTest.java
+++ src/test/java/lab2_1/BinarySearchSekwencjaTest.java
@@ -10,7 +10,7 @@ import edu.iis.mto.bsearch.SearchResult;
 public class BinarySearchSekwencjaTest {
 
 	@Test
-	public void matchingKeyShouldBeFoundInSingleElementSequence(){
+	public void KeyFoundInSec(){
 		int key = 4;
 		int[] seq = {4};
 
@@ -20,7 +20,17 @@ public class BinarySearchSekwencjaTest {
 		assertTrue(searchResult.isFound());
 		assertEquals(key, seq[i]);
 }
-	
+	@Test
+	public void KeyNotFoundInSec(){
+		int key = 3;
+		int[] seq = {4};
+
+		SearchResult searchResult = BinarySearch.search(key, seq);
+		int i = searchResult.getPosition();
+
+		assertFalse(searchResult.isFound());
+		assertEquals(-1, i);
+}
 	
 		
 	
