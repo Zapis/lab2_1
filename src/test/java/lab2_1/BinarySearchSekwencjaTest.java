013b2d0324d9515091061b5432396e37e2fc588a
Marcin Zapisek <zapisekm@gmail.com>
2016-12-17 16:03:02 +0100

    v1

--- src/test/java/lab2_1/BinarySearchSekwencjaTest.java
+++ src/test/java/lab2_1/BinarySearchSekwencjaTest.java
@@ -1,5 +1,20 @@
 package lab2_1;
 
+import org.junit.Test;
+
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
+import junit.framework.Assert;
+
 public class BinarySearchSekwencjaTest {
 
+	@Test	
+	public void test(){
+		SearchResult searchresult = new SearchResult();
+        Assert.assertEquals("czy dlugosc sekwencji wynosi 0",SearchResult(0) );
+	}
+	
+	
+		
+	
 }
