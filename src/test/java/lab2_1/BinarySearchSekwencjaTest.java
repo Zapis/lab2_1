f63950c09bc9897b55d93abb2809b87f536a3807
Marcin Zapisek <zapisekm@gmail.com>
2016-12-17 16:11:54 +0100

    v2

--- src/test/java/lab2_1/BinarySearchSekwencjaTest.java
+++ src/test/java/lab2_1/BinarySearchSekwencjaTest.java
@@ -1,18 +1,25 @@
 package lab2_1;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import edu.iis.mto.bsearch.BinarySearch;
 import edu.iis.mto.bsearch.SearchResult;
-import junit.framework.Assert;
 
 public class BinarySearchSekwencjaTest {
 
-	@Test	
-	public void test(){
-		SearchResult searchresult = new SearchResult();
-        Assert.assertEquals("czy dlugosc sekwencji wynosi 0",SearchResult(0) );
-	}
+	@Test
+	public void matchingKeyShouldBeFoundInSingleElementSequence(){
+		int key = 4;
+		int[] seq = {4};
+
+		SearchResult searchResult = BinarySearch.search(key, seq);
+		int i = searchResult.getPosition();
+
+		assertTrue(searchResult.isFound());
+		assertEquals(key, seq[i]);
+}
 	
 	
 		
