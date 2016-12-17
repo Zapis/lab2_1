a82c6ec2e0795720b3645a01e369530fcc3076f2
Marcin Zapisek <zapisekm@gmail.com>
2016-12-17 17:27:51 +0100

    v3

--- src/test/java/lab2_1/BinarySearchSekwencjaTest.java
+++ src/test/java/lab2_1/BinarySearchSekwencjaTest.java
@@ -1,34 +1,26 @@
 package lab2_1;
-
 import static org.junit.Assert.*;
-
-import org.hamcrest.Matcher;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.*;
 import edu.iis.mto.bsearch.BinarySearch;
 import edu.iis.mto.bsearch.SearchResult;
 
 public class BinarySearchSekwencjaTest {
 
-	/*@Test
+	@Test
 	public void KeyFoundInSec(){
 		int key = 4;
 		int[] seq = {4};
-
 		SearchResult searchResult = BinarySearch.search(key, seq);
 		int i = searchResult.getPosition();
-
 		assertTrue(searchResult.isFound());
 		assertEquals(key, seq[i]);
-}*/
-	/*@Test
+}
+	@Test
 	public void KeyNotFoundInSec(){
 		int key = 6;
 		int[] seq = {4};
-
-		SerchResult searchResult = BinarySearch.search(key, seq);
+		SearchResult searchResult = BinarySearch.search(key, seq);
 		int i = searchResult.getPosition();
-
 		assertFalse(searchResult.isFound());
 		assertEquals(-1, i);
 }
@@ -42,24 +34,17 @@ public class BinarySearchSekwencjaTest {
 		assertTrue(searchResult.isFound());
 		assertEquals(key, seq[i]);
 		}
-	*/
+	
 	@Test
 	public void KeyLastInSec(){
 		int key = 5;
-		int[] seq = {5,2,1,4,3};
+		int[] seq = {1,3,4,5};
 		SearchResult searchResult =BinarySearch.search(key,seq);
 		int i = searchResult.getPosition();
 		assertTrue(searchResult.isFound());
 		assertEquals(key,seq[i]);
 		}
 	
-	@Test
-		public void KeyLastInSecHeu(){
-		int key = 1;
-		int[] seq={5,2,1,4,3};
-		assertThat(key,is(seq[3]));
-	}
-	/*
 	@Test
 	public void KeyInTheMiddleInSec(){
 		int key = 4;
@@ -71,7 +56,7 @@ public class BinarySearchSekwencjaTest {
 		}
 	
 	@Test
-	public void KeyNotInSec(){
+		public void KeyNotInSec(){
 		int key = 2;
 		int[] seq = {1,3,4,5,6};
 		SearchResult searchResult =BinarySearch.search(key,seq);
@@ -79,8 +64,5 @@ public class BinarySearchSekwencjaTest {
 		assertTrue(searchResult.isFound());
 		assertEquals(key,seq[i]);
 		}
-	*/
-
-
-	}
-
+	
+}
